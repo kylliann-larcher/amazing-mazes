2ab0f7d043ee2692caa092db1a87d31d6d996d6c
Kylliann <kylliann.larcher@laplateforme.io>
2025-09-21 11:44:53 +0200

    ...

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-
-
 try:
-    # when executed as module: python -m src.main
     from .menu import run
 except Exception:
-    # when executed as script: python src/main.py
     from menu import run
 
 if __name__ == "__main__":
